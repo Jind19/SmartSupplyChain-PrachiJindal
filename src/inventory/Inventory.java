cac106fada11e2455e2c375abc59023bfaa5fea7
Prachi Jindal <prachibansal135@gmail.com>
2025-04-25 15:31:27 +0200

    added package and inventory class to display products are stored, grouped, and managed.

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -1,4 +1,40 @@
 package inventory;
 
-public class Inventory {
+import exception.ExpiredProductException;
+import exception.InvalidInputException;
+import items.Product;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Inventory<T extends Product> {
+
+    private Map<String, Package<T>> packageMap = new HashMap<>();
+
+    public void addNewPackage(String id, Package<T> pkg) throws InvalidInputException {
+        if (id == null || pkg == null) {
+            throw new InvalidInputException("ID or Package cannot be null.");
+        }
+        packageMap.put(id, pkg);
+    }
+
+    public Package<T> retrievePackageByID(int Id) throws InvalidInputException {
+        if (!packageMap.containsKey(Id)) {
+            throw new InvalidInputException("Package ID not found.");
+        }
+        return packageMap.get(Id);
+    }
+
+    public void printProductList(List<? extends Product> items) {
+        for (Product p : items) {
+            System.out.println(" - " + p);
+        }
+    }
+
+    public void validateAllPackages(int today) throws ExpiredProductException {
+        for (Package<T> pkg : packageMap.values()) {
+            pkg.validateItems(today);
+        }
+    }
 }
--- src/inventory/Package.java
+++ src/inventory/Package.java
@@ -1,4 +1,40 @@
 package inventory;
 
-public class Package {
+import exception.ExpiredProductException;
+import exception.InvalidInputException;
+import items.Perishable;
+import items.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Package<T extends Product>{
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) throws InvalidInputException {
+        if (item == null) {
+            throw new InvalidInputException("Item cannot be null.");
+        }
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void validateItems(int today) throws ExpiredProductException {
+        for (T item : items) {
+            if (item instanceof Perishable) {
+                Perishable p = (Perishable) item;
+                if (p.isExpired(today)) {
+                    throw new ExpiredProductException(item.getName() + " is expired.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Package: " + items.toString();
+    }
 }
